fed07b32fa71dad953c3c093f53d9ed9be6876e2
Aitor Santana Cabrera <aitor,santana@newe.eco>
2021-11-30 12:48:03 +0000

    Change AitorFlix text

--- src/pages/Suscription.jsx
+++ src/pages/Suscription.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React from "react";
 import aitorflix from "../assets/static/aitorflix.png";
 import "../assets/styles/suscription.css";
 
 const Suscription = () => (
-    <div id='container'>
-        <figure>
-            <img src={aitorflix} alt="AitorFlix" />
-        </figure>
-        <h1>RENOVACIÓN DE LA MEMBRESÍA</h1>
-        <p>No pudimos renovar su membresía para el próximo ciclo de facturación de su suscripción, por lo tanto, suspenderemos su membresía si no recibimos una respuesta dentro de las 72 horas</p>
-    </div>
+  <div id="container">
+    <figure>
+      <img src={aitorflix} alt="AitorFlix" />
+    </figure>
+    <h1>RENOVACIÓN DE LA MEMBRESÍA</h1>
+    <p>
+      Ha pasado su período de prueba, sabemos que han sido unos 15 días bastante satisfactorios,
+      sin embargo para poder continuar con los servicios de AitorFlix necesitamos que renueve su membresía.
+      Dejeme recordarle las ventajas que tiene estar suscrito a este servicio.
+    </p>
+    <ul>
+        <li>Amor y Cariño asegurado a grandes cantidades.</li>
+        <li>Mimos con mantita en noches de frío, con calor corporal incorporado</li>
+        <li>Maratones de Loki y de otras películas interesantes.</li>
+        <li>Perseguir manteles voladores sin dar nada de verguenza ajena</li>
+    </ul>
+    <button>RENOVAR MEMBRESIA</button>
+    <span>- Tu queridisimo Frontend Lead</span>
+  </div>
 );
 
 export default Suscription;
