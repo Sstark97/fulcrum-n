6766f090a3bbe30a5a4aef8ac69dd918372ba319
Aitor Santana Cabrera <aitor,santana@newe.eco>
2021-11-30 13:46:15 +0000

    Add navbar for confirm page

--- src/pages/ConfirmSucription.jsx
+++ src/pages/ConfirmSucription.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import aitorflix from "../assets/static/aitorflix.png";
+import "../assets/styles/confirm.css";
 
 const ConfirmSuscription = () => (
-    <div>Hola</div>
+    <div id='container'>
+        <nav>
+            <img src={aitorflix} alt='aitorflix' />
+
+            <div id='account'>
+                <span class='confirm_span'>Tu cuenta</span>
+                <span class='confirm_span'>Cerrar sesión</span>
+            </div> 
+        </nav>
+    </div>
 );
 
 export default ConfirmSuscription;
