ff82f79cebf03c407a374f1757ed105e4ed95774
Aitor Santana Cabrera <aitor,santana@newe.eco>
2021-12-01 08:14:57 +0000

    Project Structured

--- src/pages/ConfirmSucription.jsx
+++ src/pages/ConfirmSucription.jsx
@@ -13,21 +13,34 @@ const ConfirmSuscription = () => (
             </div> 
         </nav>
 
+        <h1>¡AitorFlix te da la bienvenida!</h1>
+
         <div>
-            <p></p>
-            <p></p>
-            <p></p>
+            <p>Tu suscripción a AitorFlix ha sido renovada con éxito, ahora es cuándo se vierne lo bueno.</p>
+            <p>Dudamos mucho de que quiera cancelar su suscripción, sabemos lo cómoda que se encuentra disfrutando de nuestros servicios.</p>
+            <p>Si estabas disfrutando de AitorFlix, a partir de ahora estarás más a gusto que antes, los beneficios y ventajas que tiene este servicio son inmnumerables, aunque dentro de ellas no están dejarse perder a Pokémon.</p>
         </div>
 
         <div id='account_details'>
-            <h3>r</h3>
-            <p></p>
-            <p></p>
-            <p></p>
+            <h3>Detalles de tu cuenta</h3>
+            <p>
+                <span>Nombre: </span>
+                <span>Sarita la Bobita</span>
+            </p>
+
+            <p>
+                <span>Dirección de correo: </span>
+                <span>saracs15@scssn@gmail.com</span>
+            </p>
+            
+            <p>
+                <span>Información de Pago: </span>
+                <span>MIMOS ILIMITADOS ***** 1506</span>
+            </p>
         </div>
 
         <div id='suscription'>
-            <h3>r</h3>
+            <h3></h3>
             <p></p>
         </div>
 
